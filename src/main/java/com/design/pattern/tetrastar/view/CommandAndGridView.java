fbe00a1c3fe077b8199842f5626343e03ff86157
Akshata Hajeri <ahajeri@scu.edu>
2016-09-03 15:54:47 -0700

    Added Comment
    To test the code

--- src/main/java/com/design/pattern/tetrastar/view/CommandAndGridView.java
+++ src/main/java/com/design/pattern/tetrastar/view/CommandAndGridView.java
@@ -114,7 +114,8 @@ public class CommandAndGridView extends AView {
         this.consolePanel.setPreferredSize(new Dimension(250,100));
         this.consolePanel.setSize(new Dimension(250, 100));
         JTextArea consoleLogger = new JTextArea(42, 20);
-        consoleLogger.setLineWrap(true);
+        consoleLogger.setLineWrap(true); 
+        //
         consoleLogger.setWrapStyleWord(true);
         consoleLogger.setText("");
         
